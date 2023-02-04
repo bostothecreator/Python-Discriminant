06c8888ce6a6536c28b898ea9cac4d445b62c2fe
bostothecreator <123944321+bostothecreator@users.noreply.github.com>
2023-02-05 01:48:49 +0400

    Add files via upload

---
+++ discriminant.py
@@ -0,0 +1,19 @@
+from math import sqrt   
+
+def discriminant(a,b,c):
+
+    d = b**2 - 4*a*c 
+
+    if d < 0:
+        return "idinaxui aramodis"
+    elif d == 0:
+        x= -b / (2*a)
+        return x
+    else:
+        x1 = (-b + sqrt(d)) / (2*a)
+        x2 = (-b - sqrt(d)) / (2*a)
+
+    return x1, x2 
+
+print(discriminant(2, 15, 5))
+ 
